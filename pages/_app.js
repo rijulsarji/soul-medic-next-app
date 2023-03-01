2646dca98f2a3b3f63eda24c4fb91f96c94898b5
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2023-03-02 01:56:30 +0530

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,14 @@ import { StateContext } from "../context/StateContext";
 import "../styles/globals.css";
 
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      <Head>
+        <link rel="shortcut icon" href="/Logo.ico" />
+      </Head>
       <Layout>
         <Toaster />
         <Component {...pageProps} />
